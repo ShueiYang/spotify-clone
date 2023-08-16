9b27251aed831aabced90a698ef4c3fa00521b61
ShueiYang <yangshuei83@gmail.com>
2023-08-16 21:41:15 +0200

    relocate page.tsx and update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,22 @@
+"use client"
+
 import Box from "@/components/Box";
 
 
 export default function NotFound() {
   
   return (
-    <Box className="h-full flex items-center justify-center">
-      <h1 className="text-neutral-400 text-xl">
+    <Box className="h-full flex flex-col items-center justify-center">
+      <h1 className="text-neutral-400 text-xl mb-6">
         404 This page could not be found.
       </h1>
+      <a 
+        href="/"
+        rel="noopener noreferrer"
+        className="item-link"
+      >
+        - Back to hompage -
+      </a>
     </Box>
   );
 };
