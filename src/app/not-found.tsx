13740fe0367432c41fee2442e6d66e17700914f4
ShueiYang <yangshuei83@gmail.com>
2023-08-16 14:18:11 +0200

    customize not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Box from "@/components/Box";
+
+
+export default function NotFound() {
+  
+  return (
+    <Box className="h-full flex items-center justify-center">
+      <h1 className="text-neutral-400 text-xl">
+        404 This page could not be found.
+      </h1>
+    </Box>
+  );
+};
