4b81742257c41d6860e3df3b3d2c47c98c3cc62a
ShueiYang <yangshuei83@gmail.com>
2023-08-17 03:48:44 +0200

    remove onClick in MediaItem and move to parent component

--- src/components/contents/LikedContent.tsx
+++ src/components/contents/LikedContent.tsx
@@ -27,13 +27,16 @@ export default function LikedContent({
     <div className="flex flex-col w-full gap-y-2 px-6 mb-4">
       {songs.map(song => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
-          <div className="relative group flex-1">
-            <MediaItem
-              data={song}
-              onClick={(id) => onPlay(id)}
-            />
+          <div 
+            className="relative group flex-1"
+            onClick={() => onPlay(song.id as string)}
+          >
+            <MediaItem data={song} />
             <div className="absolute right-5 inset-y-3">
-              <PlayButton className="p-3 translate-x-1/4 group-hover:translate-x-0"/>
+              <PlayButton
+                songId={song.id as string} 
+                className="p-3 translate-x-1/4 group-hover:translate-x-0"
+              />
             </div>
           </div>
           <LikeButton songId={song.id} />
--- src/components/sidebar/MediaItem.tsx
+++ src/components/sidebar/MediaItem.tsx
@@ -7,26 +7,15 @@ import { Song } from "@/types/custom.types";
 
 interface MediaItemProps {
   data: Song
-  onClick?: (id: string) => void
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({
   data,
-  onClick,
 }) => {
   const imageUrl = useLoadImageUrl(data);
 
-  function handleClick() {
-    if(onClick) {
-      return onClick(data.id as string)
-    }
-  }
-
   return (
-    <div
-      onClick={handleClick}
-      className="flex items-center w-full gap-x-3 p-2 rounded-md cursor-pointer hover:bg-neutral-800/70"
-    >
+    <div className="flex items-center w-full gap-x-3 p-2 rounded-md cursor-pointer hover:bg-neutral-800/70">
       <div className="relative min-w-[48px] min-h-[48px] rounded-md overflow-hidden">
         <Image 
           src={imageUrl || "/images/music-placeholder.png"}
