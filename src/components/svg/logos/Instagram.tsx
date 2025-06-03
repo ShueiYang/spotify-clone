e187c26c05b72300621298fab4a17db1678f3626
ShueiYang <yangshuei83@gmail.com>
2025-06-03 15:20:02 +0200

    fix(instagram-logo): fix react component styling of Instagram logo

--- src/components/svg/logos/Instagram.tsx
+++ src/components/svg/logos/Instagram.tsx
@@ -7,9 +7,9 @@ export function Instagram(props: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-instagram-icon lucide-instagram"
       {...props}
     >
